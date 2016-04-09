fed2cd9139bda0c8b15990cadc0059f607ecda3c
avtor31 <avtor31@yandex.ru>
2016-04-09 22:02:00 +0300

    1 - directive ng-repeat

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,17 +1,31 @@
 'use strict';
 
 var myApp=angular.module('myApp', []);
-myApp.controller("parentController", function ($scope) {
-    $scope.model={
-        name: "Table",
-        value: 'red'
-    }
-});
-myApp.controller("firstChildController", function ($scope) {
-    $scope.model={
-        name: "Car",
-        value: 'bmw'
-    }
-});
-myApp.controller("secondChildController", function ($scope) {
+myApp.controller('phoneController', function($scope) {
+
+    $scope.phones = [{
+        name: 'Nokia Lumia 630',
+        year: 2014,
+        price: 200,
+        company: {
+            name: 'Nokia',
+            country: '?????????'
+        }
+    },{
+        name: 'Samsung Galaxy S 4',
+        year: 2014,
+        price: 400,
+        company: {
+            name: 'Samsung',
+            country: '?????????? ?????'
+        }
+    },{
+        name: 'Mi 5',
+        year: 2015,
+        price: 300,
+        company: {
+            name: 'Xiaomi',
+            country: '?????'
+        }
+    }]
 });
